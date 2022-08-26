264016a91952a1d7ec9a1ba2890ee2b65739d448
Rauye Rogiski <8474382+rauye@users.noreply.github.com>
2022-08-26 09:32:15 -0300

    Update boleto_download.php

--- boleto_download.php
+++ boleto_download.php
@@ -3,10 +3,17 @@ require __DIR__ . '/vendor/autoload.php';
 
 use \Curl\Curl;
 
+/*
+  Informar atualizado:1 para boleto com valor atualizado
+*/
+
 $conf = require('conf.php');
 $curl = new Curl($conf->admin_url);
 $curl->setHeader('X-Requested-With', 'XMLHttpRequest');
 $curl->setBasicAuthentication($conf->username, $conf->password);
-$curl->get('/boletos/imprimir/formato:pdf/id:41214');
+$curl->get('/boletos/imprimir/formato:pdf/atualizado:0/id:41214');
 echo 'Response:' . "\n";
 var_dump(base64_encode($curl->response));
+
+
+
