df5d93f64b99a1faedaa18513acf3d638f8de704
Christiano Becker <christiano@duobox.com.br>
2023-09-15 14:00:51 -0300

    * Desfazer liquidação

--- boletos_desfazer_liquidacao.php
+++ boletos_desfazer_liquidacao.php
@@ -7,12 +7,8 @@ $conf = require('conf.php');
 $curl = new Curl($conf->admin_url);
 $curl->setHeader('X-Requested-With', 'XMLHttpRequest');
 $curl->setBasicAuthentication($conf->username, $conf->password);
-$curl->post('/clientes_cobrancas/liquidar',[
+$curl->post('/clientes_cobrancas/desfazer_liquidacao',[
     'id' => 6296, //id da cobrança/boleto
-    'valor_pago' => '1.123,45', // string
-    'data_pagamento' => '15/09/2023',
-    'forma_recebimento' => 5, // 5 - cartão de débito - para listar consulte no sistema Cadastros  -> Fluxo Financeiro -> Tipos de Pagamento
-    'gerar_recibo' => 0
 ]);
 echo 'Response:' . "\n";
 var_dump($curl->response);
