2026df7b027bbd496168b46f948ead39e9709d0f
Christiano Becker <christiano@duobox.com.br>
2024-11-01 10:00:17 -0300

    * Exemplos para retornar o base64 do pdf dos boletos

--- boleto_download.php
+++ boleto_download.php
@@ -11,6 +11,7 @@ $conf = require('conf.php');
 $curl = new Curl($conf->admin_url);
 $curl->setHeader('X-Requested-With', 'XMLHttpRequest');
 $curl->setBasicAuthentication($conf->username, $conf->password);
+// caso queira o pdf já em base64 basta trocar o formato de "pdf" para "pdf-base64"
 $curl->get('/boletos/imprimir/formato:pdf/atualizado:0/id:41214');
 echo 'Response:' . "\n";
 var_dump(base64_encode($curl->response));
--- boletos_ca.php
+++ boletos_ca.php
@@ -8,5 +8,12 @@ $curl = new Curl($conf->ca_url);
 $curl->setHeader('X-Requested-With', 'XMLHttpRequest');
 $curl->setBasicAuthentication($conf->ca_login, $conf->ca_senha);
 $curl->get('/boletos/index?json');
+// no json de resposta os boletos em aberto vem com o link_download
+// este link_download retorna o arquivo pdf
+// caso queira o base64 do pdf use a mesma url mas adicionando /formato:pdf-base64 no final dela
+// exemplo de url retornada - download do pdf:
+// https://central.demo-dev.ispbox.com.br/boletos/imprimir/id:295947/auth:15aeed34ca898982a1f45cedd6ab6d46
+// exemplo da url para retornar o base64 do pdf:
+// https://central.demo-dev.ispbox.com.br/boletos/imprimir/id:295947/auth:15aeed34ca898982a1f45cedd6ab6d46/formato:pdf-base64
 echo 'Response:' . "\n";
 var_dump($curl->response);
